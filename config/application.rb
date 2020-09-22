d903a390ca3125d3b95e5815a322c4d4407298cc
David Seeherr <david.seeherr@gmail.com>
2020-09-22 11:11:30 +0200

    Remove action_text, action_mailbox and action_storage

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,27 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all'
+# active_storage/engine
+# action_mailbox/engine
+# action_text/engine
+require 'rails'
+
+%w[
+  active_record/railtie
+  action_controller/railtie
+  action_view/railtie
+  action_mailer/railtie
+  active_job/railtie
+  action_cable/engine
+  rails/test_unit/railtie
+  sprockets/railtie
+].each do |railtie|
+  require railtie
+rescue LoadError
+  puts 'Could not load railtie'
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
