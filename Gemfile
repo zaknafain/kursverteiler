18657f489d2710aba9df680e247a1520d468c77e
David Seeherr <david.seeherr@gmail.com>
2020-09-28 12:05:56 +0200

    Add Faker gem and seed the students

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'rails_admin_import', '~> 2.2'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
+  # Generate fake data for development and testing
+  gem 'faker', '~> 2.14.0'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,15 @@ student = Student.find_or_initialize_by(email: 'foo@bar.com')
 student.assign_attributes({ first_name: 'Jane', last_name: 'Doe', password: 'foobar20',
                             password_confirmation: 'foobar20' })
 student.save!
+# Create 200 more dummy Students to fill the list
+200.times do
+  email    = Faker::Internet.safe_email
+  password = Faker::Internet.password(min_length: 8, max_length: 20, mix_case: true, special_characters: true)
+  student  = Student.find_or_initialize_by(email: email)
+  student.assign_attributes({ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+                              password: password, password_confirmation: password })
+  student.save!
+end
 
 # Creates a dummy Admin for testing
 admin = Admin.find_or_initialize_by(email: 'admin@example.com')
