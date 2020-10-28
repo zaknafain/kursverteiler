b73a830849a61742c4d1b6e24c672f841b8fd0ef
David Seeherr <david.seeherr@gmail.com>
2020-10-28 15:09:05 +0100

    Add and show selections for students

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,7 +10,10 @@ class Student < ApplicationRecord
 
   belongs_to :grade
   has_many :selections, dependent: :destroy
-  has_many :current_selections, -> { current }, class_name: 'Selection', inverse_of: 'student'
+  has_many :current_selections, -> { current },                 class_name: 'Selection', inverse_of: :student
+  has_one  :top_selection,      -> { current.top_priority },    class_name: 'Selection', inverse_of: :student
+  has_one  :mid_selection,      -> { current.medium_priority }, class_name: 'Selection', inverse_of: :student
+  has_one  :low_selection,      -> { current.low_priority },    class_name: 'Selection', inverse_of: :student
 
   validates :first_name, :last_name, presence: true
 end
--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -6,8 +6,9 @@ RSpec.describe Student, type: :model do
   let(:student) { build(:student) }
 
   context 'relations' do
-    let!(:selection)     { create(:selection, student: student) }
+    let!(:selection)     { create(:selection, student: student, poll: poll) }
     let!(:old_selection) { create(:selection, student: student, poll: old_poll) }
+    let(:poll)           { create(:poll) }
     let(:old_poll)       { create(:poll, valid_from: 18.months.ago, valid_until: 6.months.ago) }
 
     it 'destroys all its selections on deletion' do
@@ -21,6 +22,34 @@ RSpec.describe Student, type: :model do
 
       expect(student.current_selections.count).to be(1)
       expect(student.current_selections.pluck(:id)).to include(selection.id)
+    end
+
+    it 'has one current top selection' do
+      expect(student.selections.count).to be(2)
+      expect(student.selections.pluck(:id)).to include(selection.id)
+      expect(student.selections.pluck(:id)).to include(old_selection.id)
+
+      expect(student.top_selection.id).to be(selection.id)
+    end
+
+    it 'has one current mid selection' do
+      mid_selection = create(:selection, student: student, priority: 1, poll: poll)
+
+      expect(student.selections.count).to be(3)
+      expect(student.selections.pluck(:id)).to include(selection.id)
+      expect(student.selections.pluck(:id)).to include(mid_selection.id)
+
+      expect(student.mid_selection.id).to be(mid_selection.id)
+    end
+
+    it 'has one current low selection' do
+      low_selection = create(:selection, student: student, priority: 2, poll: poll)
+
+      expect(student.selections.count).to be(3)
+      expect(student.selections.pluck(:id)).to include(selection.id)
+      expect(student.selections.pluck(:id)).to include(low_selection.id)
+
+      expect(student.low_selection.id).to be(low_selection.id)
     end
   end
 
