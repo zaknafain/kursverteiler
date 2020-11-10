24b9eebb7fdb12dd2d2ffb6d07df53f9cd69d112
David Seeherr <david.seeherr@gmail.com>
2020-11-10 10:46:44 +0100

    Redirect to admins sign in after admins sign out

--- app/controllers/students/sessions_controller.rb
+++ app/controllers/students/sessions_controller.rb
@@ -29,9 +29,14 @@ module Students
       root_path
     end
 
+    def after_sign_out_path_for(_)
+      new_student_session_path
+    end
+
     # If you have extra params to permit, append them to the sanitizer.
     # def configure_sign_in_params
     #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
     # end
+
   end
 end
