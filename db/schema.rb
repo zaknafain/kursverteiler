a22185d33cc609b468afe44b8e290e219e1e47e9
David Seeherr <david.seeherr@gmail.com>
2020-11-18 13:54:13 +0100

    Add description column to polls

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_204954) do
+ActiveRecord::Schema.define(version: 2020_11_18_124420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_204954) do
     t.date "valid_until", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "selections", force: :cascade do |t|
