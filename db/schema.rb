562d2b780064c980d6b609ab658f922046136e7a
David Seeherr <david.seeherr@gmail.com>
2020-11-19 16:32:21 +0100

    Move description from courses to rt table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_103436) do
+ActiveRecord::Schema.define(version: 2020_11_19_152437) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,6 @@ ActiveRecord::Schema.define(version: 2020_11_19_103436) do
     t.string "title", null: false
     t.integer "minimum", null: false
     t.integer "maximum", null: false
-    t.text "description"
     t.string "teacher_name"
     t.boolean "guaranteed", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
