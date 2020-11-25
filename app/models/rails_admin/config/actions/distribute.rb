3067f3380024540f12bcf039075f5e1a7bff2dfb
David Seeherr <david.seeherr@gmail.com>
2020-11-25 11:57:20 +0100

    Show some first information on distribute

--- app/models/rails_admin/config/actions/distribute.rb
+++ app/models/rails_admin/config/actions/distribute.rb
@@ -15,7 +15,32 @@ module RailsAdmin
 
         register_instance_option :controller do
           proc do
+            if request.get? # SHOW
+              respond_to do |format|
+                format.html { render @action.template_name }
+                format.js   { render @action.template_name, layout: false }
+              end
+            # elsif request.put? # UPDATE
+            #   sanitize_params_for!(request.xhr? ? :modal : :update)
 
+            #   @object.set_attributes(params[@abstract_model.param_key])
+            #   @authorization_adapter.authorize(:update, @abstract_model, @object) if @authorization_adapter.present?
+            #   changes = @object.changes
+            #   if @object.save
+            #     if @auditing_adapter.present?
+            #       @auditing_adapter.update_object(@object, @abstract_model, _current_user, changes)
+            #     end
+
+            #     respond_to do |format|
+            #       format.html { redirect_to_on_success }
+            #       format.js do
+            #         render json: { id: @object.id.to_s, label: @model_config.with(object: @object).object_label }
+            #       end
+            #     end
+            #   else
+            #     handle_save_error :distribute
+            #   end
+            end
           end
         end
 
