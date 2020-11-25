23287f67bf7df71cdd4d53c1804ac1d0b0de4dad
David Seeherr <david.seeherr@gmail.com>
2020-11-25 13:21:07 +0100

    Change poll factory to be less error prone

--- spec/factories.rb
+++ spec/factories.rb
@@ -23,10 +23,10 @@ FactoryBot.define do
   end
 
   factory :poll do
-    title       { "#{Faker::Lorem.unique.word.capitalize} #{DateTime.now.strftime('%Y')}" }
-    valid_from  { 6.months.ago }
-    valid_until { 6.months.from_now }
-    description { Faker::Lorem.paragraph(sentence_count: 2) }
+    sequence(:title) { |n| "#{Faker::Lorem.word.capitalize} #{DateTime.now.strftime('%Y')}-#{n}" }
+    valid_from       { 6.months.ago }
+    valid_until      { 6.months.from_now }
+    description      { Faker::Lorem.paragraph(sentence_count: 2) }
 
     trait :ended do
       valid_from  { 18.months.ago - 1.day }
