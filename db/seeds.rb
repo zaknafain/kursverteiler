e837614c16b9d65f9de6637e55975dbd423a3462
David Seeherr <david.seeherr@gmail.com>
2020-11-25 17:11:02 +0100

    Add distribution to seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,83 +6,77 @@ end
 
 if Rails.env.development?
   log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> SEEDING <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<')
+  if Admin.count.positive?
+    log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!! DB NOT  EMPTY !!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    return
+  end
 
   # Creates a dummy Admin for testing
   log('Create a dummy Admin for testing (admin@example.com)')
-  admin = Admin.find_or_initialize_by(email: 'admin@example.com')
-  admin.assign_attributes({ first_name: 'John', last_name: 'Doe', password: 'foobar20',
-                            password_confirmation: 'foobar20' })
-  admin.save!
+  Admin.create!({ email: 'admin@example.com', first_name: 'John', last_name: 'Doe', password: 'foobar20',
+                  password_confirmation: 'foobar20' })
 
   # Create 2 dummy Polls for the courses following
   log('------------------------- Create 2  dummy Polls -------------------------')
-  [Time.zone.today.year - 1, Time.zone.today.year].each do |year|
+  polls = [Time.zone.today.year - 1, Time.zone.today.year].map do |year|
     title = "Kurswahl #{year}"
     log("Create #{title}")
-    poll = Poll.find_or_initialize_by(title: title)
-    poll.assign_attributes({ valid_from: Date.new(year),
-                             valid_until: Date.new(year, 12, 31),
-                             description: Faker::Lorem.paragraph(sentence_count: 10) })
-    poll.save!
+    Poll.create!({ title: title, valid_from: Date.new(year), valid_until: Date.new(year, 12, 31),
+                   description: Faker::Lorem.paragraph(sentence_count: 10) })
   end
 
   # Create 10 dummy Classes for the upcomming Students
   log('------------------------ Create 10 dummy Classes ------------------------')
-  10.times do |index|
+  grades = (0..9).to_a.map do |index|
     name = "Klasse #{('A'.ord + index).chr}"
     log("Create dummy #{name}")
-    grade = Grade.find_or_create_by!(name: name)
-    grade.polls << Poll.all
+
+    Grade.create!({ name: name, polls: polls })
   end
 
   # Create 20 dummy Courses to fill the list
   log('------------------------ Create 20 dummy Courses ------------------------')
-  Poll.all.each_with_index do |poll, poll_index|
+  polls.each_with_index do |poll, poll_index|
     guaranteed_course = (0..9).to_a.sample
     10.times do |index|
       title = Faker::Educator.course_name
       guaranteed = guaranteed_course == index
       course_number = ((index + 1) + (10 * poll_index))
       log("Create dummy Course #{course_number.to_s.rjust(2)} #{"(#{title})".ljust(39)} #{'guaranteed' if guaranteed}")
-      course = Course.find_or_initialize_by(title: title)
-      course.poll = poll
-      course.assign_attributes({ minimum: Faker::Number.within(range: 8..12),
-                                 maximum: Faker::Number.within(range: 14..30),
-                                 description: Faker::Lorem.paragraph(sentence_count: 10),
-                                 teacher_name: Faker::FunnyName.two_word_name,
-                                 focus_areas: Faker::Lorem.sentence(word_count: 0, random_words_to_add: 7),
-                                 variants: Faker::Lorem.sentence(word_count: 4, random_words_to_add: 7),
-                                 guaranteed: guaranteed })
-      course.save!
+      Course.create!({ title: title, poll: poll, minimum: guaranteed ? nil : Faker::Number.within(range: 8..12),
+                       guaranteed: guaranteed, maximum: guaranteed ? nil : Faker::Number.within(range: 14..30),
+                       description: Faker::Lorem.paragraph(sentence_count: 10),
+                       teacher_name: Faker::FunnyName.two_word_name,
+                       focus_areas: Faker::Lorem.sentence(word_count: 0, random_words_to_add: 7),
+                       variants: Faker::Lorem.sentence(word_count: 4, random_words_to_add: 7) })
     end
   end
 
   # Create 201 more dummy Students to fill the list
-  log('----------------------- Create 201 dummy Students -----------------------')
+  log('----------------------- Create 200 dummy Students -----------------------')
 
   # Creates first dummy Student for testing
-  log("Create dummy Student   1 #{'(foo@bar.com)'.ljust(37)} in #{Grade.first.name}")
-  foo_bar_student = Student.find_or_initialize_by(email: 'foo@bar.com')
-  foo_bar_student.assign_attributes({ first_name: 'Jane', last_name: 'Doe', password: 'foobar20',
-                                      password_confirmation: 'foobar20', grade: Grade.first })
-  foo_bar_student.save!
+  sample_grade = grades.sample
+  log("Create dummy Student   1 #{'(foo@bar.com)'.ljust(37)} in #{sample_grade.name}")
+  Student.create!({ email: 'foo@bar.com', first_name: 'Jane', last_name: 'Doe', password: 'foobar20',
+                    password_confirmation: 'foobar20', grade: sample_grade })
 
-  Grade.limit(10).each_with_index do |grade, grade_index|
-    20.times do |index|
-      email = Faker::Internet.safe_email
-      student_number = (((index + 1) + (20 * grade_index)) + 1)
+  grade_size = 20
+  grades.each_with_index do |grade, grade_index|
+    missing_students = grade_size - grade.students.length
+    missing_students.times do |index|
+      email = Faker::Internet.unique.safe_email
+      student_number = (((index + 1) + (grade_size * grade_index)) + 1)
       log("Create dummy Student #{student_number.to_s.rjust(3)} #{"(#{email})".ljust(36)} in #{grade.name}")
       password = Faker::Internet.password(min_length: 8, max_length: 20, mix_case: true, special_characters: true)
-      student  = Student.find_or_initialize_by(email: email)
-      student.assign_attributes({ first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
-                                  password: password, password_confirmation: password, grade: grade })
-      student.save!
+      Student.create!({ email: email, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
+                        password: password, password_confirmation: password, grade: grade })
     end
   end
 
   # Create Selections for all Students and Polls
   log('-------------------- Create Selections  for Students --------------------')
-  Poll.all.each do |poll|
+  polls.each do |poll|
     Student.all.each do |student|
       selection_amount = (0..3).to_a.sample
       selected_courses = poll.courses.where(guaranteed: false).order(Arel.sql('RANDOM()')).first(selection_amount)
@@ -100,6 +94,43 @@ if Rails.env.development?
       guaranteed_course = poll.courses.find_by(guaranteed: true)
       selection.update!(top_course: guaranteed_course)
       log("Student #{student.id.to_s.rjust(3)} selected guaranteed course: #{guaranteed_course.title}")
+    end
+  end
+
+  # Create distribution for all Polls from the past
+  log('------------------ Create Distribution  for past Polls ------------------')
+  polls.select{ |p| p.valid_until < Time.zone.now }.each do |poll|
+    log("Starting Distribution for poll #{poll.title}")
+    not_distributed_students = []
+
+    poll.students.includes(selections: %i[top_course mid_course low_course]).each do |student|
+      top_course = student.current_selection&.top_course
+      mid_course = student.current_selection&.mid_course
+      low_course = student.current_selection&.low_course
+      if top_course&.guaranteed
+        log("Distribute #{student.full_name} to guaranteed #{top_course.title}")
+        student.courses_students.create!(course: top_course)
+      elsif top_course && top_course.students.length < top_course.maximum
+        log("Distribute #{student.full_name} to 1st choice #{top_course.title}")
+        student.courses_students.create!(course: top_course)
+      elsif mid_course && mid_course.students.length < mid_course.maximum
+        log("Distribute #{student.full_name} to 2nd choice #{mid_course.title}")
+        student.courses_students.create!(course: mid_course)
+      elsif low_course && low_course.students.length < low_course.maximum
+        log("Distribute #{student.full_name} to 3rd choice #{low_course.title}")
+        student.courses_students.create!(course: low_course)
+      else
+        not_distributed_students << student
+      end
+    end
+
+    not_distributed_students.each do |student|
+      poll.courses.each do |course|
+        if course.guaranteed == false && course.students.length < course.maximum
+          log("Distribute #{student.full_name} to #{course.title}")
+          student.courses_students.create!(course: course)
+        end
+      end
     end
   end
 elsif Rails.env.production?
