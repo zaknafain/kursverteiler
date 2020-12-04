3c4cccab6290ae6594056a156c844a12b56cf099
David Seeherr <david.seeherr@gmail.com>
2020-12-04 11:48:17 +0100

    Allow selecting of students

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w(rails_admin/themes/bs30/*)
+Rails.application.config.assets.precompile += %w(rails_admin/themes/bs30/* rails_admin/custom/*)
