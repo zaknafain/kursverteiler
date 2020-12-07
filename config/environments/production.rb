876e6fd35053736caf6e4b68ecbd6c6fa4f84556
David Seeherr <david.seeherr@gmail.com>
2020-12-07 13:59:08 +0100

    Use port 25 and no ssl for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,10 +71,9 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
-    port: 465,
+    port: 25,
     user_name: Rails.application.credentials.sendgrid['user_name'],
     password: Rails.application.credentials.sendgrid['password'],
-    ssl: true
   }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
