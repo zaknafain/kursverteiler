082bdd494b6668f0deff496ca73937a7dc68c640
David Seeherr <david.seeherr@gmail.com>
2020-12-07 13:39:41 +0100

    Add sendgrid to producion action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,17 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'bs30.de', port: 80 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.sendgrid.net',
+    port: 465,
+    user_name: Rails.application.credentials.sendgrid['user_name'],
+    password: Rails.application.credentials.sendgrid['password'],
+    ssl: true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
