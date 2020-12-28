53f9e836ed2a60504b358c4b673918afa10f50b7
David Seeherr <david.seeherr@gmail.com>
2020-12-28 21:56:35 +0100

    Change students show to use bootstrap

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -11,24 +11,4 @@ module StudentsHelper
 
     "alert alert-#{map[flash_type.to_sym]}"
   end
-
-  def course_classes(prio_selected, disabled)
-    classes = ['course']
-
-    classes << 'course__selected'                   if prio_selected.present?
-    classes << "course__selected--#{prio_selected}" if prio_selected.present?
-    classes << 'course__disabled'                   if disabled
-
-    classes.join(' ')
-  end
-
-  def course_prio_classes(priority, selected, guaranteed, disabled)
-    classes = ['course-priority--container']
-
-    classes << "course-priority--container__#{priority}"
-    classes << 'course-priority--container__selected' if selected
-    classes << 'course-priority--container__disabled' if disabled || guaranteed && %i[mid low].include?(priority)
-
-    classes.join(' ')
-  end
 end
