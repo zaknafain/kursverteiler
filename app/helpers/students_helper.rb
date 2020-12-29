24a82d9acead3e34942c5aa4d481996653449b16
David Seeherr <david.seeherr@gmail.com>
2020-12-29 11:07:23 +0100

    Show which course is selected

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -11,4 +11,15 @@ module StudentsHelper
 
     "alert alert-#{map[flash_type.to_sym]}"
   end
+
+  def course_card_classes(selected_priority)
+    classes = %w[card h-100]
+
+    classes << 'bg-light'   if selected_priority.nil?
+    classes << 'bg-success' if selected_priority == :top
+    classes << 'bg-warning' if selected_priority == :mid
+    classes << 'bg-danger'  if selected_priority == :low
+
+    classes.join(' ')
+  end
 end
