481e0d697d4bb0bd6a0fd8bea3681488a59f1d97
David Seeherr <david.seeherr@gmail.com>
2020-12-31 10:50:01 +0100

    Finalize prod and dev settings for mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,9 +36,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.smtp_settings = { address: '127.0.0.1', port: 1025 }
+  # config.action_mailer.default_url_options = { host: 'https://kursverteiler.herokuapp.com', port: 443 }
   # config.action_mailer.smtp_settings = {
   #   address: 'smtp.sendgrid.net',
   #   authentication: :plain,
