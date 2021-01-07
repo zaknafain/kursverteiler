9bd1e10ba3e66f341ed41afe1f9251e35b9c1d41
David Seeherr <david.seeherr@gmail.com>
2021-01-07 20:39:49 +0100

    Fix missing image in mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,6 +67,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: ENV['MAILER_HOST'], port: 443 }
+  config.action_mailer.asset_host = ENV['MAILER_HOST']
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
