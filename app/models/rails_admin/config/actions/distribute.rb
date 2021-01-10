3c598da10d48b26124c87224439620435e15533a
David Seeherr <david.seeherr@gmail.com>
2021-01-10 22:39:00 +0100

    Add card with completion to distribution

--- app/models/rails_admin/config/actions/distribute.rb
+++ app/models/rails_admin/config/actions/distribute.rb
@@ -27,19 +27,30 @@ module RailsAdmin
 
               render @action.template_name
             elsif request.put? # UPDATE
-              sanitized_params = params.require(:poll).permit(courses_students: %i[student_id course_id])
+              sanitized_params = params.require(:poll).permit(:completed, courses_students: %i[student_id course_id])
 
-              sanitized_params[:courses_students].each do |(_, param)|
-                student = students.detect { |s| s.id == param[:student_id].to_i }
-                student.courses = student.courses.reject do |course|
-                  @courses.map(&:id).include?(course.id) && param[:course_id].to_i != course.id
+              if sanitized_params[:courses_students].present?
+                sanitized_params[:courses_students].each do |(_, param)|
+                  student = students.detect { |s| s.id == param[:student_id].to_i }
+                  student.courses = student.courses.reject do |course|
+                    @courses.map(&:id).include?(course.id) && param[:course_id].to_i != course.id
+                  end
+                  if param[:course_id].present? && student.courses.map(&:id).exclude?(param[:course_id].to_i)
+                    student.courses << @courses.detect { |c| c.id == param[:course_id].to_i }
+                  end
                 end
-                if param[:course_id].present? && student.courses.map(&:id).exclude?(param[:course_id].to_i)
-                  student.courses << @courses.detect { |c| c.id == param[:course_id].to_i }
+                @courses.reload
+              elsif sanitized_params[:completed]
+                @object.update(completed: Time.zone.now)
+                @object.courses.includes(:students).find_each do |course|
+                  course.students.each do |student|
+                    StudentsMailer.with(student: student, poll: @object, course: course)
+                                  .inform_about_distribution
+                                  .deliver_now
+                  end
                 end
               end
               @not_dist_students = students.not_distributed_in(@object.id).order(last_name: :asc, first_name: :asc)
-              @courses.reload
 
               render @action.template_name
             end
