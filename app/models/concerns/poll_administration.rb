418289282c38ab2efebf79648b7e2fcf114fcc44
David Seeherr <david.seeherr@gmail.com>
2021-01-12 21:33:11 +0100

    Add students without selection to administration

--- app/models/concerns/poll_administration.rb
+++ app/models/concerns/poll_administration.rb
@@ -26,6 +26,26 @@ module PollAdministration
       end
       show do
         field :grades
+        field :students_without_selection do
+          pretty_value do
+            if value
+              view = bindings[:view]
+              grade_id = nil
+              links = value.sort_by { |s| [s.grade_id] }.each_with_object([]) do |student, link_list|
+                url = view.rails_admin.show_path(model_name: 'student', id: student.id)
+
+                link_list << view.tag.br if student.grade_id != grade_id && grade_id.present?
+                link_list << view.tag.span("#{student.grade.name}: ") if student.grade_id != grade_id
+                link_list << view.link_to(student&.to_pretty_value, url, class: 'pjax')
+                link_list << ', '
+                grade_id = student.grade_id
+              end
+              view.safe_join(links)
+            else
+              '-'
+            end
+          end
+        end
       end
       edit do
         field :grades do
