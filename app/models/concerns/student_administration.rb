a0ebf9693b7b8d57b73b622592a4f70c38ebc623
David Seeherr <david.seeherr@gmail.com>
2021-01-26 23:27:57 +0100

    Remove selections from student edit form

--- app/models/concerns/student_administration.rb
+++ app/models/concerns/student_administration.rb
@@ -32,20 +32,6 @@ module StudentAdministration
             default_value ''
           end
         end
-        %i[top mid low].each do |priority|
-          field :"current_#{priority}_course" do
-            visible { bindings[:object].current_poll.present? }
-            inline_add false
-            inline_edit false
-            associated_collection_cache_all false
-            associated_collection_scope do
-              student = bindings[:object]
-              proc do |scope|
-                scope.where(poll: student.current_poll.id).order(:title)
-              end
-            end
-          end
-        end
       end
       import do
         mapping_key :email
@@ -60,15 +46,5 @@ module StudentAdministration
 
   def before_import_save(_)
     self.password = self.password_confirmation = Devise.friendly_token(50) unless persisted?
-  end
-
-  %i[top mid low].each do |priority|
-    define_method(:"current_#{priority}_course_id") do
-      public_send(:"current_#{priority}_course")&.id
-    end
-
-    define_method(:"current_#{priority}_course_id=") do |id|
-      public_send(:"current_#{priority}_course=", current_poll&.courses&.find_by(id: id))
-    end
   end
 end
