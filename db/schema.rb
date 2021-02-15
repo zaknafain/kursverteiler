29686eb9a9deeb10168fb5efb8d0f2fa3ed85ec0
David Seeherr <david.seeherr@gmail.com>
2021-02-15 21:38:55 +0100

    Add valid until to grades

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_26_224828) do
+ActiveRecord::Schema.define(version: 2021_02_15_203640) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2021_01_26_224828) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "valid_until"
     t.index ["name"], name: "index_grades_on_name", unique: true
   end
 
