31b6d460a0aa9029cc32e657298a8dbfa1403dc2
David Seeherr <david.seeherr@gmail.com>
2024-09-09 15:15:40 +0200

    Add coordinators flag to admins

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_18_194201) do
+ActiveRecord::Schema.define(version: 2024_09_09_131325) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2021_05_18_194201) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "first_name", null: false
     t.string "last_name", null: false
+    t.boolean "coordinator", default: false, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
