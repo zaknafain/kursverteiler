b85ddaa0aa526dbcb987f38970076a1fc00490d7
David Seeherr <david.seeherr@gmail.com>
2025-04-11 17:49:24 +0200

    Make the visuals for selections react to the user clicking

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -5,8 +5,8 @@ module StudentsHelper
   def course_card_classes(selected_priority, course_disabled)
     classes = %w[divide-y divide-gray-200 overflow-hidden rounded-lg bg-white shadow-sm mb-4]
 
-    classes << 'text-gray-500'                          if course_disabled
-    classes << priority_color_border(selected_priority) if selected_priority
+    classes << 'text-gray-500'                 if course_disabled
+    classes << "selected #{selected_priority}" if selected_priority
 
     classes.join(' ')
   end
@@ -15,26 +15,24 @@ module StudentsHelper
     tag.button(
       type: 'button',
       class: priority_button_classes(priority),
-      data: priority_button_data(course, priority, selected_prio)
+      data: priority_button_data(course, priority)
     ) do
       priority_button_content(priority, selected_prio == priority)
     end
   end
 
   def priority_button_content(priority, selected)
-    return tag.span(t("students.show.prio.#{priority}")) unless selected
-
     safe_join(
       [
         tag.div(t("students.show.prio.#{priority}")),
-        check_icon
+        check_icon(selected)
       ]
     )
   end
 
-  def check_icon
-    tag.svg(class: '-ml-0.5 size-5', viewBox: '0 0 20 20', fill: 'currentColor', 'aria-hidden': 'true',
-            'data-slot': 'icon') do
+  def check_icon(selected)
+    tag.svg(class: "#{'hidden' unless selected} -ml-0.5 size-5", viewBox: '0 0 20 20', fill: 'currentColor',
+            'aria-hidden': 'true', 'data-slot': 'icon') do
       tag.path('fill-rule': 'evenodd', 'clip-rule': 'evenodd',
                d: 'M10 18a8 8 0 1 0 0-16 8 8 0 0 0 0 16Zm3.857-9.809a.75.75 0 0 0-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 1 0-1.06 1.061l2.5 2.5a.75.75 0 0 0 1.137-.089l4-5.5Z') # rubocop:disable Layout/LineLength
     end
@@ -48,12 +46,8 @@ module StudentsHelper
     classes.join(' ')
   end
 
-  def priority_button_data(course, priority, selected_prio)
+  def priority_button_data(course, priority)
     {
-      course_id: course.id,
-      priority: priority,
-      selected_prio: selected_prio,
-      guaranteed: course.guaranteed?,
       selection_form_course_id_param: course.id,
       selection_form_course_prio_param: priority,
       action: 'click->selection-form#toggle'
@@ -65,26 +59,6 @@ module StudentsHelper
       top: 'bg-emerald-50 text-emerald-600 hover:bg-emerald-100',
       mid: 'bg-amber-50 text-amber-600 hover:bg-amber-100',
       low: 'bg-rose-50 text-rose-600 hover:bg-rose-100'
-    }
-
-    map[priority]
-  end
-
-  def priority_color_text(priority)
-    map = {
-      top: 'text-emerald-600',
-      mid: 'text-amber-600',
-      low: 'text-rose-600'
-    }
-
-    map[priority]
-  end
-
-  def priority_color_border(priority)
-    map = {
-      top: 'border-emerald-600 ring-2 ring-emerald-600',
-      mid: 'border-amber-600 ring-2 ring-amber-600',
-      low: 'border-rose-600 ring-2 ring-rose-600'
     }
 
     map[priority]
